e82fc32c0f337a8418d6b190f40d4b5b676be92a
asatriangrish <grishasatrian9@gmail.com>
2021-05-24 00:13:27 +0400

    modified filter and sort menu and fixed several functionalities

--- src/app/shared/components/sort-modal/sort-modal.component.spec.ts
+++ src/app/shared/components/sort-modal/sort-modal.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SortModalComponent } from './sort-modal.component';
 
-describe('FilterModalComponent', () => {
+describe('SortModalComponent', () => {
   let component: SortModalComponent;
   let fixture: ComponentFixture<SortModalComponent>;
 
